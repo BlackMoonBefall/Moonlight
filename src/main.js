61ac220a92861ec50871cb68be5cbcc85e62bb6f
Wise Yan <catchthemoon@qq.com>
2020-08-22 14:30:31 +0800

    '初始化'

--- src/main.js
+++ src/main.js
@@ -6,3 +6,18 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+
+const a = {
+  aaa(){
+    setTimeout(function(){
+      console.log(this);
+    })
+  },
+
+  bbb(){
+    setTimeout(() => {
+      console.log(this);
+    })
+  }
+}
